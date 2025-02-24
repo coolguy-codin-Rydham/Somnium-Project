eca8831c7f289401e4ba853192d5715454907179
rupesh0105 <rupeshsingla786@gmail.com>
2025-02-24 15:10:40 +0530

    Authentication pagecommit 2

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Signup from "./Components/Signup.jsx";
+import AuthForm from "./Components/Signin.jsx"
 
 function App() {
   return (
     <>
       <Signup/>
+      <AuthForm/>
     </>
   );
 }
